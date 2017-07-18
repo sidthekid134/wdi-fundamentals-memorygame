bc9b6a5c62b095f80761256735421d099f39541b
Moparthi <gdk510@dca90475e8ee.cof.ds.capitalone.com>
2017-07-18 15:20:03 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running!")
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
