ffc1bf777fe18d992fd995bf134790cb45625349
Moparthi <gdk510@dca90475e8ee.cof.ds.capitalone.com>
2017-07-18 16:39:23 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-console.log("Up and Running!")
+/*
+  Author: Siddhardha Moparthi
+  Capital One: Memory Game
+*/
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+// Initializing variables
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }else{
+    alert("Sorry, try again.");
+  }
+}
