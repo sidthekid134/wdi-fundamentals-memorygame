03e8cb5542da82b6c47a8284002d0f6da8b262d3
Moparthi <gdk510@dca90475e8ee.cof.ds.capitalone.com>
2017-07-19 10:29:08 -0400

    started game logic

--- js/main.js
+++ js/main.js
@@ -6,18 +6,32 @@
 // Initializing variables
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
-  if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+// Checks to see if the two cards flipped are the same
+var checkForMatch = function (){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
   }else{
-    alert("Sorry, try again.");
+    console.log("Sorry, try again.");
   }
 }
+
+// Does the processing when a user flips a card
+var flipCard = function (cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+
+  // if(cardsInPlay.length === 2){
+  //   if(cardsInPlay[0] === cardsInPlay[1]){
+  //     alert("You found a match!");
+  //   }else{
+  //     alert("Sorry, try again.");
+  //   }
+  // }
+
+}
+
+
+flipCard(0);
+flipCard(1);
